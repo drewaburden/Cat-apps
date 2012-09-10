f3cddb1855c4c7a0cc0904db7c31e2dd1b9fbf16
Drew Burden <dremth@gmail.com>
2012-09-10 19:52:21 -0400

    Simple menu functionality implemented
    Implemented some simple menu functionality. Clicking on the Settings
    menu option in the main activity will take you to the settings activity
    (which is currently a blank activity).

--- Cat-apps/src/com/hogg/catapps/MainActivity.java
+++ Cat-apps/src/com/hogg/catapps/MainActivity.java
@@ -2,7 +2,9 @@ package com.hogg.catapps;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +18,17 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public boolean onOptionsItemSelect(MenuItem item) {
+    	// Handle item selection
+        switch (item.getItemId()) {
+	        case R.id.menu_settings:
+	        	Intent myIntent = new Intent(this, SettingsActivity.class);
+	            startActivity(myIntent);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+        }        
     }
 }
