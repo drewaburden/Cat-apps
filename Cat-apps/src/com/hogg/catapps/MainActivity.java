cd2931d35a72d280970e97ab7fa2aaff69c3f9b9
yoharnu <dani91@gmail.com>
2012-09-12 17:49:28 -0400

    fixed spaces/tabs

--- Cat-apps/src/com/hogg/catapps/MainActivity.java
+++ Cat-apps/src/com/hogg/catapps/MainActivity.java
@@ -10,60 +10,60 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    public void onOptionsItemSelect(MenuItem item) {
-    	// Handle item selection
-        switch (item.getItemId()) {
-        	// Settings option
-	        case R.id.menu_settings:
-	        	// Start the settings activity
-	        	Intent myIntent = new Intent(this, SettingsActivity.class);
-	            startActivity(myIntent);
-	            break;
-	            
-	        default:
-	           break;
-        }
-    }
-    
-    public void onCatButtonClick(View view) {
-    	// Find the TextView so we can manipulate it
-    	final TextView text = (TextView) findViewById(R.id.textView1);
-    	
-    	// Only if the TextView is invisible
-    	if (text.getVisibility() != View.VISIBLE) {
-    		// Make it visible
-    		text.setVisibility(View.VISIBLE);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    		// Wait for 2 seconds (using threads so we don't freeze the UI)
-    		// and then make the text invisible again.
-    		// Todo: implement a better sleep system using a class?
-    		new Thread(new Runnable() {
-    		    public void run() {
-    		        try {
-    		            Thread.sleep(2000);
-    		        } catch (InterruptedException e) {
-    		            e.printStackTrace();
-    		        }
-    		        runOnUiThread(new Runnable() {
-    		            public void run() {
-    		        		text.setVisibility(View.INVISIBLE);
-    		            }
-    		        });
-    		    }
-    		}).start();
-    		
-    	}
-    }
+	public void onOptionsItemSelect(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		// Settings option
+		case R.id.menu_settings:
+			// Start the settings activity
+			Intent myIntent = new Intent(this, SettingsActivity.class);
+			startActivity(myIntent);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public void onCatButtonClick(View view) {
+		// Find the TextView so we can manipulate it
+		final TextView text = (TextView) findViewById(R.id.textView1);
+
+		// Only if the TextView is invisible
+		if (text.getVisibility() != View.VISIBLE) {
+			// Make it visible
+			text.setVisibility(View.VISIBLE);
+
+			// Wait for 2 seconds (using threads so we don't freeze the UI)
+			// and then make the text invisible again.
+			// Todo: implement a better sleep system using a class?
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					runOnUiThread(new Runnable() {
+						public void run() {
+							text.setVisibility(View.INVISIBLE);
+						}
+					});
+				}
+			}).start();
+
+		}
+	}
 }
