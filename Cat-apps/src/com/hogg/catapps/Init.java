a912b2d3424ec5b4bf72e0fbdb40bd44806a9451
yoharnu <dani91@gmail.com>
2012-11-26 12:33:59 -0500

    Fix merge remnants

--- Cat-apps/src/com/hogg/catapps/Init.java
+++ Cat-apps/src/com/hogg/catapps/Init.java
@@ -31,12 +31,8 @@ public class Init extends Application {
 <<<<<<< HEAD
 	public static Cat cat; // Temporary. This holds our single cat (globally accessible). Later, activity Player variable will be define here which will hold an ArrayList of Cats.
 	public static Player player; // Temporary.
-	static Context context; // The stored Application's Context as activity global variable 
-=======
-	public static Cat cat; // Temporary. This holds our single cat (globally accessible). Later, a Player variable will be define here which will hold an ArrayList of Cats.
 	static Context context; // The stored Application's Context as a global variable 
 	public static Thread simulation;
->>>>>>> Simulation Push 1
 
 	// This runs only when the app is started from scratch.
 	// Essentially, this only happens when we don't already have the variables seen above initialized.
@@ -47,16 +43,12 @@ public class Init extends Application {
         context = getApplicationContext();
 		
 		// Set up our cat and start tracking and updating its meters
-<<<<<<< HEAD
-		cat = new Cat(getString(R.string.default_cat_name), Sex.MALE, 0, 100, 50, 50);
 		player = new Player("");
-=======
         simulation = new Thread(new Simulation());
         simulation.start();
 		
         cat = new Cat(getString(R.string.default_cat_name), Sex.MALE, 0, 100, 50, 50);
 		
->>>>>>> Simulation Push 1
 	}
 	
 	// Retrieve the Application's Context from anywhere
